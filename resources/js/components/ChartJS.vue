f65ca4a8abd65d11564c255cb5310ded8832d7fc
developermauricio <70330439+developermauricio@users.noreply.github.com>
2021-11-11 16:06:16 -0500

    first commit

---
+++ resources/js/components/ChartJS.vue
@@ -0,0 +1,39 @@
+<template>
+  <canvas ref="graphic" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  name: "CharJS",
+  props: {
+    config: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  created() {
+    this.chart = null;
+  },
+  mounted() {
+    this.drawChart();
+  },
+  watch: {
+    config: function () {
+      this.drawChart();
+    },
+  },
+  methods: {
+    drawChart() {
+      if (this.chart) {
+        this.chart.destroy();
+      }
+      const chart = new this.$chart(this.$refs.graphic, this.config);
+      this.$emit("init", chart);
+      this.chart = chart;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
